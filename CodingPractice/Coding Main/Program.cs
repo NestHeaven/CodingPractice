7c06ad39198080c546055869fdb85b59fd0dcc15
Harsh Gupta <h.gupta@globant.com>
2024-10-05 11:57:51 +0530

    Added a program for reverse a string

--- CodingPractice/Coding Main/Program.cs
+++ CodingPractice/Coding Main/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Coding_Main.Programs;
+
+//Q.1: How to reverse a string?
+//string str = Console.ReadLine();
+
+//ReverseString reverseString = new ReverseString();
+
+//Console.WriteLine(reverseString.GetReversedStringApproach1(str));
+//Console.WriteLine(reverseString.GetReversedStringApproach2(str));
---
+++ CodingPractice/Coding Main/Programs/ReverseString.cs
@@ -0,0 +1,27 @@
+﻿namespace Coding_Main.Programs
+{
+    public class ReverseString
+    {
+        public string GetReversedStringApproach1(string str)
+        {
+            string result = string.Empty;
+
+            foreach (char c in str) {
+
+                result = $"{c}{result}";
+            }
+            return result;
+        }
+
+        public string GetReversedStringApproach2(string str)
+        {
+            string result = string.Empty;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+    }
+}
