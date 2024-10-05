ff2e2626ba99eadb3ac27db7e0a25e9c9b262009
Harsh Gupta <h.gupta@globant.com>
2024-10-05 12:37:01 +0530

    Program to check Palindrome

--- CodingPractice/Coding Main/Program.cs
+++ CodingPractice/Coding Main/Program.cs
@@ -9,3 +9,12 @@ using Coding_Main.Programs;
 
 //Console.WriteLine(reverseString.GetReversedStringApproach1(str));
 //Console.WriteLine(reverseString.GetReversedStringApproach2(str));
+
+
+//Q.2: How to find if the given string is a palindrome or not?
+
+//string str = Console.ReadLine();
+
+//Palindrome palindrome = new Palindrome();
+
+//Console.WriteLine(palindrome.CheckPalindrome(str));
---
+++ CodingPractice/Coding Main/Programs/Palindrome.cs
@@ -0,0 +1,26 @@
+﻿namespace Coding_Main.Programs
+{
+    internal class Palindrome
+    {
+
+        public string CheckPalindrome(string palindrome) { 
+            
+            bool isPalidrome = false;
+
+            for (int i = 0, j = palindrome.Length - 1; i < palindrome.Length /2; i++, j--) {
+                if (palindrome[i] != palindrome[j])
+                {
+                    isPalidrome = false;
+                    break;
+                }
+                else
+                    isPalidrome = true;
+            }
+
+            if (isPalidrome)
+                return "Palindrome";
+            else
+                return "Not Palindrome";
+        }
+    }
+}
