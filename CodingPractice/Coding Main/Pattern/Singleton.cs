876500052a04788b6aca95406307b75f78636781
Harsh Gupta <h.gupta@globant.com>
2024-10-06 22:25:08 +0530

    Implementation for singleton Pattern

---
+++ CodingPractice/Coding Main/Pattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace Coding_Main.Pattern
+{
+    public class Singleton
+    {
+        private static Singleton? instance;
+
+        private static readonly object _lock = new();
+        public static Singleton Instance { get
+            {
+                if (instance == null)
+                {
+                    lock (_lock)
+                    {
+                        instance = new Singleton();
+                    }
+                    
+                }
+                return instance;
+            }
+        }
+        public Singleton()
+        {
+            
+        }
+
+        public void PrintSingleton() => Console.WriteLine("Singleton");
+    }
+}
