213703e0cc7abec7e02ff359142609978e79eeed
Harsh Gupta <h.gupta@globant.com>
2025-03-12 15:29:51 +0530

    Added a program to check pattern is valid or not

---
+++ CodingPractice/Coding Main/Programs/ValidatePattern.cs
@@ -0,0 +1,38 @@
+﻿namespace Coding_Main.Programs
+{
+    public class ValidatePattern
+    {
+        public static bool ValidateThePattern(string pattern)
+        {
+            List<char> chars = [];
+
+            bool isValidPattern = true;
+
+            for (int i = 0; i <= pattern.Length - 1; i++)
+            {
+                if (pattern[i] == '{' && (i + 1) <= pattern.Length - 1 && pattern[i + 1] == '}')
+                {
+                    i ++;
+                    continue;
+                }
+                else if (pattern[i] == '{')
+                {
+                    chars.Add(pattern[i]);
+                }
+                else if (pattern[i] =='}' && chars.Count>0)
+                {
+                    chars.Remove('{');
+                }
+                else if(pattern[i] == '}' && chars.Count <= 0)
+                {
+                    isValidPattern = false;
+                }
+            }
+
+            if (chars.Count > 0) { 
+                isValidPattern = false;
+            }
+            return isValidPattern; 
+        }
+    }
+}
