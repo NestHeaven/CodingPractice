e807fae08b9503e88fa3aa289a18cbe42c719916
Harsh Gupta <h.gupta@globant.com>
2025-03-23 12:28:32 +0530

    CircularLinked List

---
+++ CodingPractice/Coding Main/DataStructure/LinkedList/CircularLinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System.Globalization;
+
+namespace Coding_Main.DataStructure.LinkedList
+{
+    public class CircularLinkedList
+    {
+        private Node? Head;
+        private Node? Tail;
+        private int Size;
+
+        public CircularLinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+
+        
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+
+
+        public void InsertElementAtTheEnd(int value)
+        {
+            Node newest = new Node(value, null);
+
+            if (IsEmpty()) { 
+                Head = newest;
+            }
+            else
+            {
+                Tail.Next = newest;
+            }
+
+            Tail = newest;
+            Tail.Next = Head;
+
+            Size++;
+        }
+
+        public void TraverseCircularLinkedList()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+
+            var p = Head;
+
+            do
+            {
+                Console.Write($"{p.Element}");
+                p = p.Next;
+            }
+            while (p != Head);
+
+        }
+        public void TraverseCircularLinkedListBySize()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+
+            var p = Head;
+            int i = 0;
+
+            while (i < Size)
+            {
+                Console.Write($"{p.Element} --->");
+                p = p.Next;
+                i++;
+            }
+        }
+    }
+}
