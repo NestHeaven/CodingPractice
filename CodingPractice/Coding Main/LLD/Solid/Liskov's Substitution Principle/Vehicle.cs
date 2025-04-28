b52cb6c11f35945b1d779353d8137f04577be22a
Harsh Gupta <h.gupta@globant.com>
2025-04-28 17:47:15 +0530

    Implementing LSP

---
+++ CodingPractice/Coding Main/LLD/Solid/Liskov's Substitution Principle/Vehicle.cs
@@ -0,0 +1,45 @@
+﻿namespace Coding_Main.LLD.Solid.Liskov_s_Substitution_Principle
+{
+    public class Vehicle
+    {
+        public virtual void Accelerate()
+        {
+            Console.WriteLine("It accelerate");
+        }
+    }
+
+    public class MoterCycle: Vehicle
+    {
+        public override void Accelerate() {
+            Console.WriteLine("Bike can accerate");
+        }
+    }
+
+    public class Bycycle : Vehicle
+    {
+        // voilating Lsp (base class should extend the parent class not narrow it down)
+        public override void Accelerate()
+        {
+            throw new NotImplementedException("It can't accelerate");
+        }
+    }
+
+
+
+    //What we can do to follow LSP 
+
+
+    public class SuperVehicle : Vehicle
+    {
+
+    }
+
+
+    public class NormalVehicle
+    {
+
+    }
+
+
+    // Now the bycycle class can inherit NormalVehicle not Vehicle which follows LSP
+}
