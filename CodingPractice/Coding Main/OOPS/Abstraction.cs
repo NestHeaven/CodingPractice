72beec6fe57106ea083044905d775195736eb078
Harsh Gupta <h.gupta@globant.com>
2025-07-24 17:29:12 +0530

    Encapsulation and Abstraction

---
+++ CodingPractice/Coding Main/OOPS/Abstraction.cs
@@ -0,0 +1,22 @@
+﻿namespace Coding_Main.OOPS
+{
+
+    // Abstraction → Hides implementation details and shows only necessary functionality(e.g., abstract classes, interfaces).
+
+    //The user of IMessageService does not care how Send is implemented.
+
+    //Tomorrow you can create SmsService without changing existing code.
+
+
+    public interface INotificationService
+    {
+        public abstract void SendNotification();
+    }
+    public class EmailService : INotificationService
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("Email Notification Send");
+        }
+    }
+}
---
+++ CodingPractice/Coding Main/OOPS/Encapsulation.cs
@@ -0,0 +1,33 @@
+﻿namespace Coding_Main.OOPS
+{
+    public  class Encapsulation
+    {
+        private string Name;
+
+        private Logger logger;
+        public Encapsulation() => logger = new Logger();
+
+        // Here encapsualation comes in Play. We have a controlled access to the private propery, So its not
+
+        // accessible from outside and a public property with the controlled setter which can prevent and validate
+
+        // the value before assigning or we can do something before returning value
+
+        public string MyProperty { get { logger.ConsoleMessage("Accessed Name"); return Name; }
+            set {
+                if(!String.IsNullOrEmpty(value)){
+                    Name = value;
+                }
+            }
+        }
+    }
+
+
+    public class Logger
+    {
+        public void ConsoleMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
